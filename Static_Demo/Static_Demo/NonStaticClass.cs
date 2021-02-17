5d4c7822da52be220e4c26dc84087d9a668dc089
johndward01 <johndward01@gmail.com>
2021-02-16 18:04:37 -0600

    initial commit

---
+++ Static_Demo/Static_Demo/NonStaticClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Demo
+{
+    class NonStaticClass 
+    {
+        private static readonly DateTime _date;
+
+        public int Age { get; set; }
+
+        public DateTime Date { get; set; }
+
+        static NonStaticClass()
+        {
+            _date = DateTime.Now;
+        }
+
+        public static void SetDate(DateTime date)
+        {
+            var n = new NonStaticClass();
+            n.Date = date;
+        }
+    }
+}
---
+++ Static_Demo/Static_Demo/StaticClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Demo
+{
+    public static class StaticClass
+    {
+        public static int Balance { get; set; } = 100;
+
+        public static int MyProperty { get; set; }        
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("I am a static method");
+        }
+
+
+    }
+}
